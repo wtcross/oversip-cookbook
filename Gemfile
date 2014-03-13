dbfc6e9701a4a14386ed17bbcb5822f0530f94f1
Tyler Cross <tcross@bandwidth.com>
2014-03-13 15:28:02 -0400

    Updated metadata

--- Gemfile
+++ Gemfile
@@ -1,11 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'berkshelf'
-gem 'chefspec'
+
 gem 'foodcritic', '~> 3.0'
 gem 'thor-foodcritic', '~> 1.1'
-gem 'rake'
-gem 'rspec'
 
 group :integration do
 	gem 'test-kitchen'
--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      "Installs/Configures OverSIP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-recipe "freeswitch", "Install OverSIP"
-recipe "freeswitch::package", "Install OverSIP from a package"
-recipe "freeswitch::tarball", "Install OverSIP from tarball"
+recipe "oversip", "Install OverSIP"
+recipe "oversip::package", "Install OverSIP from a package"
+recipe "oversip::tarball", "Install OverSIP from tarball"
 
 depends 'apt', '~> 2.2'
 
